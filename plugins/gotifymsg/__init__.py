65393a12ff048f7d4a77e5a137c544b25a40cb9b
HXJJoker <132528193+HXJJoker@users.noreply.github.com>
2024-05-05 21:56:45 +0800

    Create __init__.py

---
+++ plugins/gotifymsg/__init__.py
@@ -0,0 +1,229 @@
+import requests
+
+from app.plugins import _PluginBase
+from app.core.event import eventmanager, Event
+from app.schemas.types import EventType, NotificationType
+from typing import Any, List, Dict, Tuple
+from app.log import logger
+
+
+class GotifyMsg(_PluginBase):
+    # 插件名称
+    plugin_name = "gotify消息通知"
+    # 插件描述
+    plugin_desc = "支持使用gotify发送消息通知。"
+    # 插件图标
+    plugin_icon = "gotify.png"
+    # 插件版本
+    plugin_version = "1.0"
+    # 插件作者
+    plugin_author = "HXJJoker"
+    # 作者主页
+    author_url = "https://github.com/HXJJoker"
+    # 插件配置项ID前缀
+    plugin_config_prefix = "gotifymsg_"
+    # 加载顺序
+    plugin_order = 25
+    # 可使用的用户级别
+    auth_level = 1
+
+    # 私有属性
+    _enabled = False
+    _token = None
+    _msgtypes = []
+
+    def init_plugin(self, config: dict = None):
+        if config:
+            self._enabled = config.get("enabled")
+            self._url = config.get("url")
+            self._token = config.get("token")
+            self._priority = config.get("priority")
+            self._msgtypes = config.get("msgtypes") or []
+
+    def get_state(self) -> bool:
+        return self._enabled and (True if self._token else False)
+
+    @staticmethod
+    def get_command() -> List[Dict[str, Any]]:
+        pass
+
+    def get_api(self) -> List[Dict[str, Any]]:
+        pass
+
+    def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
+        """
+        拼装插件配置页面，需要返回两块数据：1、页面配置；2、数据结构
+        """
+        # 编历 NotificationType 枚举，生成消息类型选项
+        MsgTypeOptions = []
+        for item in NotificationType:
+            MsgTypeOptions.append({
+                "title": item.value,
+                "value": item.name
+            })
+        return [
+            {
+                'component': 'VForm',
+                'content': [
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 6
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
+                                            'model': 'enabled',
+                                            'label': '启用插件',
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VTextField',
+                                        'props': {
+                                            'model': 'url',
+                                            'label': 'url',
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VTextField',
+                                        'props': {
+                                            'model': 'priority',
+                                            'label': 'priority',
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VTextField',
+                                        'props': {
+                                            'model': 'token',
+                                            'label': 'token',
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSelect',
+                                        'props': {
+                                            'multiple': True,
+                                            'chips': True,
+                                            'model': 'msgtypes',
+                                            'label': '消息类型',
+                                            'items': MsgTypeOptions
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                ]
+            }
+        ], {
+            "enabled": False,
+            'token': '',
+            'msgtypes': []
+        }
+
+    def get_page(self) -> List[dict]:
+        pass
+
+    @eventmanager.register(EventType.NoticeMessage)
+    def send(self, event: Event):
+        """
+        消息发送事件
+        """
+        if not self.get_state():
+            return
+
+        if not event.event_data:
+            return
+
+        msg_body = event.event_data
+        # 渠道
+        channel = msg_body.get("channel")
+        if channel:
+            return
+        # 类型
+        msg_type: NotificationType = msg_body.get("type")
+        # 标题
+        title = msg_body.get("title")
+        # 文本
+        text = msg_body.get("text")
+
+        if not title and not text:
+            logger.warn("标题和内容不能同时为空")
+            return
+
+        if (msg_type and self._msgtypes
+                and msg_type.name not in self._msgtypes):
+            logger.info(f"消息类型 {msg_type.value} 未开启消息发送")
+            return
+        
+        url = f'{self._url}/message?token={self._token}'
+        _message = {"title": title, "message": text, "priority": self._priority}
+        response = requests.post(url, data=_message).json()
+
+        if response.get("id"):
+            logger.info("gotify消息发送成功")
+        else:
+            logger.info("gotify消息发送失败")
+
+
+    def stop_service(self):
+        """
+        退出插件
+        """
+        pass
